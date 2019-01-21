8945bfa24cf7ce4be6464e85750bf44eac8f273f
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-21 15:37:29 +0100

    Add nodemon for hot reloading, morgan for logging

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const heroRoutes = require('./api/routes/heroMain');
 const disabledRoutes = require('./api/routes/disabledMain');
 
+app.use(morgan('dev'));
+
 app.use('/hero-main', heroRoutes);
 app.use('/disabled-main', disabledRoutes);
 
-module.exports = app; 
+module.exports = app;
