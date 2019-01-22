eb62501a066c02a85c7d1c12de762f0d019f0cda
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-22 12:41:45 +0100

    Add request of a new ActiveCase creation (and get all needers auxiliary request)

--- api/routes/neederMain.js
+++ api/routes/neederMain.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /needer-main'
-    });
-});
+const ActiveCase = require('../models/activeCase');
+const User = require('../models/user');
+const mongoose = require('mongoose');
 
 router.get('/:dialogueId', (req, res, next) => {
     res.status(200).json({
@@ -15,13 +12,41 @@ router.get('/:dialogueId', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const helpRequest = {
-        message: req.body.message,
-        location: req.body.location
-    }
-    res.status(201).json({
-        message: 'Help request has created',
-        helpRequest: helpRequest
+    const activeCase = new ActiveCase({
+        _id: new mongoose.Types.ObjectId(),
+        neederId: req.body.userId,
+        heroId: null,
+        description: req.body.description,
+        done: false
+    });
+    activeCase
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Help request has created',
+            activeCase: result
+        });
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.get('/', (req, res, next) => {
+    Needer.find().exec()
+    .then(result => {
+        console.log(result);
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
