5790100f5f5240689146817edcc9788424ccf408
Senseinvader <d.ivantsov1983@gmail.com>
2019-01-23 11:56:26 +0100

    Add method to needer-main route: PATCH/caseId to mark case as done, add helper method to incr hero level as case marked done

--- api/routes/neederMain.js
+++ api/routes/neederMain.js
@@ -4,6 +4,7 @@ const ActiveCase = require('../models/activeCase');
 const User = require('../models/user');
 const mongoose = require('mongoose');
 
+//Method to GET all ActiveCases in the application
 router.get('/', (req, res, next) => {
     ActiveCase.find()
     .exec()
@@ -12,11 +13,11 @@ router.get('/', (req, res, next) => {
             count: results.length,
             activeCases: results.map(result => {
                 return {
+                    _id: result._id,
                     description: result.description,
                     done: result.done,
                     neederId: result.neederId,
                     heroId: result.heroId,
-                    id: result._id,
                     request: {
                         type: 'GET',
                         message: 'The link to see details of the case',
@@ -28,7 +29,7 @@ router.get('/', (req, res, next) => {
         if(results.length > 0) {
             res.status(200).json({
                 message: 'List of activeCases has fetched',
-                activeCases: response
+                response: response
             });
         } else {
             res.status(200).json({
@@ -44,19 +45,29 @@ router.get('/', (req, res, next) => {
     });
 });
 
+
+// Method to PATCH - change the status of an ActiveCase "done" to true - mark case as accomplished
 router.patch('/:activeCaseId', (req, res, next) => {
-    ActiveCase.findByIdAndUpdate({_id: req.params.activeCaseId}, {done: true}, {new: true})
+    ActiveCase.findByIdAndUpdate(
+        {
+                _id: req.params.activeCaseId, 
+                neederId: req.body.neederId,
+                heroId: req.body.heroId
+        },
+        {done: true}, 
+        {new: true})
     .exec()
     .then(result => {
-        console.log(result);
+        console.log('result', result)
+        increaseHeroLevel(result.heroId);
         res.status(201).json({
             message: 'The case was marked as as Done',
             case: {
+                _id: result._id,
                 description: result.description,
                 done: result.done,
                 neederId: result.neederId,
                 heroId: result.heroId,
-                id: result._id,
                 request: {
                     type: 'GET',
                     message: 'The link to see all active cases',
@@ -73,23 +84,18 @@ router.patch('/:activeCaseId', (req, res, next) => {
     });
 });
 
+//Method to GET the information about a particular ActiveCase by it's ID
 router.get('/:activeCaseId', (req, res, next) => {
     ActiveCase.find({_id: req.params.activeCaseId})
     .exec()
     .then(result => {
+        if (!result) {
+            res.status(404).json({
+                message: 'No such case in database'
+            });
+        }
         res.status(200).json({
-            case: {
-                description: result.description,
-                done: result.done,
-                neederId: result.neederId,
-                heroId: result.heroId,
-                id: result._id,
-                request: {
-                    type: 'GET',
-                    message: 'The link to see all active cases',
-                    url: 'http://localhost:3000/needer-main/'
-                }
-            }
+            case: result
         });
     })
     .catch(err => {
@@ -99,6 +105,7 @@ router.get('/:activeCaseId', (req, res, next) => {
     });
 });
 
+//Method to POST - create a new ActiveCase
 router.post('/', (req, res, next) => {
     const activeCase = new ActiveCase({
         _id: new mongoose.Types.ObjectId(),
@@ -148,5 +155,27 @@ router.get('/', (req, res, next) => {
         });
     });
 });
+
+function increaseHeroLevel(heroId) {
+    User.findById(
+        {_id: heroId}
+    ).exec()
+    .then(result => {
+        return result;
+    })
+    .then(result => {
+        let newLevel = result.level + 1;
+        User.update({_id: heroId}, {level: newLevel})
+        .exec()
+        .then(result => {
+            return result;
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            error:err
+        })
+    });
+}
 
 module.exports = router;
