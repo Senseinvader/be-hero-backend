9086d1cb5a5e3b6249434dfb055a14d5716a0b93
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-18 11:08:22 +0100

    Create socket instance in server module, create socketManager module

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const http = require('http');
 const app = require('./app');
+const socketIo = require('socket.io');
 
 
 const port = process.env.PORT || 4000;
 
 const server = http.createServer(app);
+const io = module.exports.io = socketIo(server);
+const socketManager = require('./socketManager');
+
+io.on('connection', socketManager => {
+  console.log('Do some stuff');
+  //  Doing things using socket object and emit method (like emitting new messages)
+  socket.on('disconnect', () => console.log('Client disconnetcted'))
+});
 
 server.listen(port);
---
+++ socketManager.js
@@ -0,0 +1,7 @@
+const io = require('./server').io;
+
+const {USER_CONNECTED, USER_DISCONNECTED, MESSAGE_RECIEVED, MESSAGE_SENT, TYPING, CHAT} = require('./Events')
+
+module.exports = function(socket) {
+  
+}
