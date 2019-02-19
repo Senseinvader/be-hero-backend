3c29d04978eab52a5e4466123b19e6f83af02259
wluka85 <wluka85@gmail.com>
2019-02-19 10:23:31 +0100

    Add function getCases and implement example socket

--- socketManager.js
+++ socketManager.js
@@ -1,7 +1,39 @@
 const io = require('./server').io;
+const ActiveCase = require('./api/models/activeCase');
 
 const {USER_CONNECTED, USER_DISCONNECTED, MESSAGE_RECIEVED, MESSAGE_SENT, TYPING, CHAT} = require('./Events')
+let connectedUsers = [];
 
 module.exports = function(socket) {
-  
+    console.log(socket.id, socket)
+    socket.on('action', (action) => {
+        if(action.type === 'server/hello'){
+          console.log('Got hello data!', action.data);
+          socket.emit('action', {type:'message', data:'good day!'});
+        }
+      });
+}
+
+const getCases = () => {
+    ActiveCase.find({heroId: null})
+    .exec()
+    .then(results => {
+        const response = {
+            count: results.length,
+            activeCases: results.map(result => {
+                return {
+                    _id: result._id,
+                    description: result.description,
+                    done: result.done,
+                    neederId: result.neederId,
+                    heroId: result.heroId
+                }
+            })
+        }
+
+        return response;
+    })
+    .catch(err => {
+        console.log(err);
+    });
 }
