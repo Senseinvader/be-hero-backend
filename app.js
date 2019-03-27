ab0565e873103d190a2872b50fc0507de1da0e2c
Senseinvader <d.ivantsov1983@gmail.com>
2019-03-27 22:07:44 +0100

    Add heroku mongo uri to mongoose connect

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const neederRoutes = require('./api/routes/neederMain');
 const signinRoutes = require('./api/routes/signIn');
 const signupRoutes = require('./api/routes/signUp');
 
-mongoose.connect(
+mongoose.connect( process.env.MONGODB_URI ||
     'mongodb://node-app:' + 
     process.env.MONGO_ATLAS_PW + 
     '@node-app-shard-00-00-nwfq3.mongodb.net:27017,node-app-shard-00-01-nwfq3.mongodb.net:27017,node-app-shard-00-02-nwfq3.mongodb.net:27017/test?ssl=true&replicaSet=node-app-shard-0&authSource=admin&retryWrites=true',
