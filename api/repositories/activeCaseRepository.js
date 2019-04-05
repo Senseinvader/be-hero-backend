bddee20f99dd786c6edb7416a73ecb7b48f33c25
wluka85 <wluka85@gmail.com>
2019-04-05 10:35:38 +0200

    Implement ActiveCaseRepository class which manage active case data and comunicate with mongoDB

---
+++ api/repositories/activeCaseRepository.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const ActiveCase = require('../models/activeCase');
+
+function ActiveCaseRepository() {
+    this.getFreeCases = () => {
+        return ActiveCase.find({heroId: null}).exec();
+    }
+
+    this.getUserCases = (userId, role) => {
+        return ActiveCase.find( role === 'hero' ? { heroId : userId } : { neederId : userId } );
+    }
+
+    this.addMessage = (message) => {
+        return ActiveCase.findOne({_id: message.caseId})
+            .exec()
+            .then(activeCase => {
+            activeCase.dialog.push(message);
+            activeCase.save();
+            })
+    }
+
+    this.createActiveCase = (description, user) => {
+        const activeCase = new ActiveCase({
+            _id: new mongoose.Types.ObjectId(),
+            neederId: user.id,
+            neederLogin: user.login,
+            heroId: null,
+            description: description,
+            done: false,
+            dialog: [],
+            timeStamp: new Date(Date.now()),
+            personalData: user.description
+          });
+          return activeCase.save();
+    }
+
+    this.markCaseTaken = (caseId, heroId) => {
+        return ActiveCase.findOneAndUpdate(
+            {
+                $and: [
+                    {_id: caseId}, {done: false}, { heroId: null }
+                ]
+            },
+            {heroId: heroId},
+            {new: true}
+        )
+        .exec()
+    }
+}
+
+module.exports = ActiveCaseRepository;
